bb40336b8dfaa2c44a7eb216e02b15a0e6c4dc6d
Emhu19 <85075226+Emhu19@users.noreply.github.com>
2024-03-31 15:15:39 +0200

    Delete skb directory

--- skb/skb/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello Miss ZOGHLAMI. J'ai fini les exos du TP donc je fais du python.")
-
-def inscription(request):
-    return HttpResponse("Maxime faut rajouter le CSS du bouton inscription")
